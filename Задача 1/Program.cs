ceda81c2acde13128f3f8f76401a4c248e08ccd0
Oleg <o.antonenko2011@yandex.ru>
2022-10-27 22:05:06 +0300

    Задача 1

---
+++ Задача 1/Program.cs
@@ -0,0 +1,22 @@
+﻿//тут уже сложна
+
+int a = new Random().Next(1, 20);
+Console.WriteLine (a);
+int b = new Random().Next(1, 20);
+Console.WriteLine (b);
+
+
+if (a == b*b)
+{
+
+Console.WriteLine ("Является");
+
+}
+else
+{
+
+Console.Write ("Не является");
+
+}
+
+
