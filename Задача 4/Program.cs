0011011cca020a7d96e8dddac055f41b3f47d760
Oleg <o.antonenko2011@yandex.ru>
2022-10-27 22:06:11 +0300

    Задача 4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+
+
+
+//Написать программу вычисления значения функции y = f(a)
+//Допустим функция y = x+1
+
+int y = 0;
+int x = new Random().Next(1, 1000);
+Console.WriteLine (x);
+
+y = x + 1;
+
+Console.WriteLine (y);
+
